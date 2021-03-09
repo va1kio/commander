7d51e3c789797fbc507298617cb57dbeed44caa7
va1kio <40730127+va1kio@users.noreply.github.com>
2021-03-09 01:04:31 +0000

    #7 and credit list

---
+++ src/ServerScriptService/Server/SystemPackages/Credits.lua
@@ -0,0 +1,9 @@
+local module = {
+	"nana_kon - Project lead",
+	"NuclearTheNoob - Maintainer",
+	"matsumooo - Maintainer",
+	"rgieseke/textredux - Matcher.lua",
+	"built with Rojo"
+}
+
+return module
