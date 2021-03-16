d74c5266696218283c9097ab8091250bc6808625
ccuser44 <68124053+ccuser44@users.noreply.github.com>
2021-03-16 17:28:44 +0200

    Update Health.lua

--- src/ServerScriptService/Server/Packages/Health.lua
+++ src/ServerScriptService/Server/Packages/Health.lua
@@ -12,10 +12,10 @@ module.Execute = function(Client, Type, Attachment)
 			return
 		end
 
-		local char = module.API.getCharacter(module.API.getPlayerWithName(Attachment))
+		local _, Humanoid = module.API.getCharacter(module.API.getPlayerWithName(Attachment))
 		
-		if char then
-			char.Humanoid.Health = tonumber(Input)
+		if Humanoid then
+			Humanoid.Health = tonumber(Input)
 			return true
 		end
 	end
