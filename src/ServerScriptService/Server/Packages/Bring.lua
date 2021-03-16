2b7c74ca5001493b32509ae73c631d6e8a1c934e
ccuser44 <68124053+ccuser44@users.noreply.github.com>
2021-03-16 23:26:44 +0200

    Update Bring.lua

--- src/ServerScriptService/Server/Packages/Bring.lua
+++ src/ServerScriptService/Server/Packages/Bring.lua
@@ -7,13 +7,11 @@ local module = {
 module.Execute = function(Client, Type, Attachment)			
 	if Type == "command" then
 		local char = module.API.getCharacter(module.API.getPlayerWithName(Attachment))
-		if char then
-			if Client.Character then
-				local primaryPart, primaryPart2 = char.PrimaryPart, Client.Character.PrimaryPart
-				if primaryPart and primaryPart2 then
-					char:SetPrimaryPartCFrame(primaryPart2.CFrame:ToWorldSpace(CFrame.new(Vector3.new(0, 0, 5))))
-					return true
-				end
+		if char and Client.Character then
+			local primaryPart, primaryPart2 = char.PrimaryPart, Client.Character.PrimaryPart
+			if primaryPart and primaryPart2 then
+				char:SetPrimaryPartCFrame(primaryPart2.CFrame:ToWorldSpace(CFrame.new(Vector3.new(0, 0, 5))))
+				return true
 			end
 		end
 	end
