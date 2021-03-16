c811f9ffbe3c522ab8b721d87dc89c915b739daa
ccuser44 <68124053+ccuser44@users.noreply.github.com>
2021-03-16 23:15:56 +0200

    Create RemoveTools.lua
    Removes the tools of the player

---
+++ src/ServerScriptService/Server/Packages/RemoveTools.lua
@@ -0,0 +1,17 @@
+local module = {
+	Name = "Removetools",
+	Description = "Removes the tools of the player",
+	Location = "Player",
+}
+
+module.Execute = function(Client, Type, Attachment)
+	if Type == "command" then
+		local Player = module.API.getPlayerWithName(Attachment)
+		local BackPack = Player:FindFirstChildOfClass("Backpack")
+		if Backpack then
+			Backpack:ClearAllChildren()
+		end
+	end
+end
+
+return module
