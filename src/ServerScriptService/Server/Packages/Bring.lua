eff7e85fb96ec26bf6c5fa14d62110310bf60962
ccuser44 <68124053+ccuser44@users.noreply.github.com>
2021-03-17 08:59:44 +0200

    Update Bring.lua

--- src/ServerScriptService/Server/Packages/Bring.lua
+++ src/ServerScriptService/Server/Packages/Bring.lua
@@ -9,7 +9,7 @@ module.Execute = function(Client, Type, Attachment)
 		local char = module.API.getCharacter(module.API.getPlayerWithName(Attachment))
 		local primaryPart, primaryPart2 = char and char.PrimaryPart, Client.Character and Client.Character.PrimaryPart
 		if primaryPart and primaryPart2 then
-			char:SetPrimaryPartCFrame(primaryPart2.CFrame:ToWorldSpace(CFrame.new(Vector3.new(0, 0, 5))))
+			char:SetPrimaryPartCFrame(primaryPart2.CFrame:ToWorldSpace(CFrame.new(Vector3.new(0, 0, 5), primaryPart2.Position)))
 			return true
 		end
 		
