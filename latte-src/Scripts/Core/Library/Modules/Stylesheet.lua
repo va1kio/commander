70becd5bfc21cb39456b4fcd9c6a49db3d2e6f8c
va1kio <40730127+va1kio@users.noreply.github.com>
2021-03-29 10:37:08 +0000

    Reorganised Stylesheet.lua

--- latte-src/Scripts/Core/Library/Modules/Stylesheet.lua
+++ latte-src/Scripts/Core/Library/Modules/Stylesheet.lua
@@ -1,13 +1,23 @@
 local module = {}
 
+-- Primary color
 module.ThemeColor = Color3.fromRGB(64, 157, 130)
+
+-- Button color
+module.Button = {}
+module.Button.RoundHoverColor = Color3.fromRGB(0, 0, 0)
+module.Button.MenuHoverColor = module.RoundButtonHoverColor
+module.Button.ModalHoverColor = module.ThemeColor
+
+-- Menu constructor colors
+module.Menu = {}
+module.Menu.DefaultTextColor = Color3.fromRGB(100, 100, 100)
+module.Menu.ExitColor = module.Menu.DefaultTextColor
+
+-- Duration for tweens
 module.Duration = {
 	VeryShort = 0.15,
 	Short = 0.3
 }
-
-module.Menu = {}
-module.Menu.DefaultTextColor = Color3.fromRGB(100, 100, 100)
-module.Menu.ExitColor = module.Menu.DefaultTextColor
 
 return module
