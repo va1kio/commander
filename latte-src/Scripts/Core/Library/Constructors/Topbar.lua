c98b942030a73a1bb4380f5edc8dbf0b5c2af2fa
va1kio <40730127+va1kio@users.noreply.github.com>
2021-03-29 10:43:51 +0000

    Topbar constructor now respects Stylesheet

--- latte-src/Scripts/Core/Library/Constructors/Topbar.lua
+++ latte-src/Scripts/Core/Library/Constructors/Topbar.lua
@@ -22,11 +22,13 @@ local Buttons = {
 module.setup = function()
 	Elements = module.Elements
 	Latte = module.Latte
-	
+	Elements.Panel.Container.Top.Background.ImageColor3 = Latte.Modules.Stylesheet.Window.TopbarColor
+	Elements.Panel.Container.Top.Title.TextColor3 = Latte.Modules.Stylesheet.Window.TopbarElementsColor
 	for i,v in pairs(Buttons) do
 		local ActualSide = Elements.Panel.Container.Top:FindFirstChild(v.Side)
 		local Comp = Latte.Components.RoundButton.new(i, v.Image, ActualSide, v.Callback)
 		Comp.LayoutOrder = v.Position
+		Comp.Image.ImageColor3 = Latte.Modules.Stylesheet.Window.TopbarElementsColor
 		Comp = nil
 	end
 end
