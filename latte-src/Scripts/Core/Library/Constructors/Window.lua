7fc99a444f1bd163b524ef09828e4edf28e75b62
va1kio <40730127+va1kio@users.noreply.github.com>
2021-03-29 09:20:19 +0000

    Window addition

--- latte-src/Scripts/Core/Library/Constructors/Window.lua
+++ latte-src/Scripts/Core/Library/Constructors/Window.lua
@@ -1,11 +1,22 @@
-local module, Latte, Elements = {}, nil, nil
+local module, Latte, Elements, isActive, currentPage = {}, nil, nil, true, nil
 
 module.Toggle = function()
+	if isActive then
+		Latte.Modules.Animator.Window.animateOut(Elements.Panel, Elements.Panel.UIScale)
+	else
+		Latte.Modules.Animator.Window.animateIn(Elements.Panel, Elements.Panel.UIScale)
+	end
 	
+	isActive = not isActive
 end
 
 module.SwitchPage = function(Page: string)
-	warn(Page)
+	if currentPage then
+		Latte.Modules.Animator.Window.animateOut(currentPage, currentPage.UIScale)
+	end
+	
+	currentPage = Elements.Panel.Container.Body[Page]
+	Latte.Modules.Animator.Window.animateIn(currentPage, currentPage.UIScale)
 end
 
 module.setup = function()
