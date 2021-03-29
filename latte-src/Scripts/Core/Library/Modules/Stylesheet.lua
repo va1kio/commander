7a26c083819aca56d0cb45a08f71292febfecc80
va1kio <40730127+va1kio@users.noreply.github.com>
2021-03-29 10:40:46 +0000

    Added Window colors and Menu.BackgroundColor

--- latte-src/Scripts/Core/Library/Modules/Stylesheet.lua
+++ latte-src/Scripts/Core/Library/Modules/Stylesheet.lua
@@ -3,6 +3,14 @@ local module = {}
 -- Primary color
 module.ThemeColor = Color3.fromRGB(64, 157, 130)
 
+-- Window color
+module.Window = {}
+module.Window.TopbarColor = module.ThemeColor
+module.Window.TopbarElementsColor = Color3.fromRGB(255, 255, 255)
+module.Window.AccentColor = Color3.fromRGB(205, 205, 205)
+module.Window.BackgroundColor = Color3.fromRGB(240, 240, 240)
+module.Window.ScrollBarColor = Color3.fromRGB(0, 0, 0)
+
 -- Button color
 module.Button = {}
 module.Button.RoundHoverColor = Color3.fromRGB(0, 0, 0)
@@ -11,6 +19,7 @@ module.Button.ModalHoverColor = module.ThemeColor
 
 -- Menu constructor colors
 module.Menu = {}
+module.Menu.BackgroundColor = Color3.fromRGB(250, 250, 250)
 module.Menu.DefaultTextColor = Color3.fromRGB(100, 100, 100)
 module.Menu.ExitColor = module.Menu.DefaultTextColor
 
