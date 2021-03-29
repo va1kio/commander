0c73019c6b4d72439f4e19524f0a9882275b8f70
va1kio <40730127+va1kio@users.noreply.github.com>
2021-03-29 14:35:46 +0000

    added newButton

--- latte-src/Scripts/Core/Library/Constructors/Menu.lua
+++ latte-src/Scripts/Core/Library/Constructors/Menu.lua
@@ -1,5 +1,4 @@
 local module, Latte, Elements = {}, nil, nil
-local ButtonNames = {"Home", "Players", "Server", "Donate", "About"}
 local Buttons = {}
 local isActive = false
 
@@ -13,6 +12,26 @@ module.Toggle = function()
 	isActive = not isActive
 end
 
+module.newButton = function(Name: string, Position: number)
+	if not Buttons[Name] then
+		Buttons[Name] = {}
+		
+		local Comp = Latte.Components.MenuButton.new(Name, Name, Elements.Panel.Container.Menu.Container.List, function()
+			module.Toggle()
+			Latte.Constructors.Window.SwitchPage(Name)
+			for i,v in pairs() do
+				if i ~= Name then
+					v.Toggle(false)
+				end
+			end
+		end)
+		
+		Buttons[Name].Toggle = Comp.setActive
+		Comp.Object.LayoutOrder = Position
+		Comp = nil
+	end
+end
+
 module.setup = function()
 	Elements = module.Elements
 	Latte = module.Latte
@@ -20,31 +39,6 @@ module.setup = function()
 	Exit.Image.ImageColor3 = Latte.Modules.Stylesheet.Menu.ExitColor
 	Elements.Panel.Container.Menu.container.Top.Accent.BackgroundColor3 = Latte.Modules.Stylesheet.Window.AccentColor
 	Elements.Panel.Container.Menu.Container.BackgroundColor3 = Latte.Modules.Stylesheet.Menu.BackgroundColor
-	
-	for i,v in pairs(ButtonNames) do
-		Buttons[v] = {
-			Position = i,
-			Callback = function()
-				Latte.Constructors.Window.SwitchPage(v)
-			end,
-		}
-	end
-	
-	for i,v in pairs(Buttons) do
-		local Comp = Latte.Components.MenuButton.new(i, i, Elements.Panel.Container.Menu.Container.List, function()
-			module.Toggle()
-			v.Callback()
-			for i2, v2 in pairs(Buttons) do
-				if i2 ~= i then
-					v2.Toggle(false)
-				end
-			end
-		end)
-		
-		v.Toggle = Comp.setActive
-		Comp.Object.LayoutOrder = v.Position
-		Comp = nil
-	end
 end
 
 return module
