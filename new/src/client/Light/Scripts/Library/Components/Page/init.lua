630291ba3e6d6212993454358ca2ee848365351e
va1kio <40730127+va1kio@users.noreply.github.com>
2021-03-30 10:40:10 +0000

    Fix

--- new/src/client/Light/Scripts/Library/Components/Page/init.lua
+++ new/src/client/Light/Scripts/Library/Components/Page/init.lua
@@ -3,7 +3,7 @@ local module = {}
 module.new = function(Name: string, Parent: instance)
 	local Comp = script.Comp:Clone()
 	Comp.Name = Name
-	Comp.VerticalScrollBarInset = Enum.VerticalScrollBarInset.ScrollBar
+	Comp.VerticalScrollBarInset = Enum.ScrollBarInset.ScrollBar
 	Comp.Parent = Parent
 	
 	return Comp
