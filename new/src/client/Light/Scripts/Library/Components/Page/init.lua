e5013dfc90aa80a08b24488d1b82b792c336883e
va1kio <40730127+va1kio@users.noreply.github.com>
2021-03-30 10:39:23 +0000

    Vertical scroll bar not overlaying fix

--- new/src/client/Light/Scripts/Library/Components/Page/init.lua
+++ new/src/client/Light/Scripts/Library/Components/Page/init.lua
@@ -3,6 +3,7 @@ local module = {}
 module.new = function(Name: string, Parent: instance)
 	local Comp = script.Comp:Clone()
 	Comp.Name = Name
+	Comp.VerticalScrollBarInset = Enum.VerticalScrollBarInset.ScrollBar
 	Comp.Parent = Parent
 	
 	return Comp
