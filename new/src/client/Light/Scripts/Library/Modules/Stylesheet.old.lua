0c55276b0759d388280e97038c43c9cb5bf2b75b
va1kio <40730127+va1kio@users.noreply.github.com>
2021-03-31 02:46:04 +0000

    Stylesheet addition, set dark stylesheet by default

--- new/src/client/Light/Scripts/Library/Modules/Stylesheet.old.lua
+++ new/src/client/Light/Scripts/Library/Modules/Stylesheet.old.lua
@@ -7,47 +7,47 @@ module.ThemeColor = Color3.fromRGB(64, 157, 130)
 module.Window = {}
 module.Window.TopbarColor = module.ThemeColor
 module.Window.TopbarElementsColor = Color3.fromRGB(255, 255, 255)
-module.Window.AccentColor = Color3.fromRGB(45, 45, 45)
-module.Window.BackgroundColor = Color3.fromRGB(25, 25, 25)
-module.Window.ScrollBarColor = Color3.fromRGB(250, 250, 250)
-module.Window.TopbarBackgroundColorIfAccentUsed = Color3.fromRGB(35,35,35)
+module.Window.AccentColor = Color3.fromRGB(205, 205, 205)
+module.Window.BackgroundColor = Color3.fromRGB(240, 240, 240)
+module.Window.ScrollBarColor = Color3.fromRGB(0, 0, 0)
+module.Window.TopbarBackgroundColorIfAccentUsed = Color3.fromRGB(255, 255, 255)
 module.Window.TopbarElementColorIfAccentUsed = Color3.fromRGB(150, 150, 150)
 module.Window.TopbarUseAccentInstead = true
 
 -- Button color
 module.Button = {}
-module.Button.RoundHoverColor = Color3.fromRGB(100, 100, 100)
+module.Button.RoundHoverColor = Color3.fromRGB(0, 0, 0)
 module.Button.MenuHoverColor = module.Button.RoundHoverColor
 module.Button.ModalHoverColor = module.ThemeColor
 
 -- Menu constructor color
 module.Menu = {}
-module.Menu.BackgroundColor = Color3.fromRGB(35, 35, 35)
-module.Menu.DefaultTextColor = Color3.fromRGB(125, 125, 125)
+module.Menu.BackgroundColor = Color3.fromRGB(250, 250, 250)
+module.Menu.DefaultTextColor = Color3.fromRGB(100, 100, 100)
 module.Menu.ExitColor = module.Menu.DefaultTextColor
 
 -- Home constructor color
 module.Home = {}
-module.Home.UsernameColor = Color3.fromRGB(250, 250, 250)
-module.Home.RankColor = Color3.fromRGB(150, 150, 150)
+module.Home.UsernameColor = Color3.fromRGB(0, 0, 0)
+module.Home.RankColor = Color3.fromRGB(120, 120, 120)
 module.Home.AvatarBorder = module.Window.AccentColor
-module.Home.AvatarBackground = Color3.fromRGB(30, 30, 30)
+module.Home.AvatarBackground = Color3.fromRGB(255, 255, 255)
 module.Home.TopImage = "rbxasset://textures/AvatarEditorImages/AvatarEditor_LightTheme.png"
 module.Home.TopUseAccentInstead = true
-module.Home.TopBackgroundColor = Color3.fromRGB(30, 30, 30)
+module.Home.TopBackgroundColor = Color3.fromRGB(250, 250, 250)
 
 -- SeparatedList component color
 module.SeparatedList = {}
 module.SeparatedList.Item = {}
-module.SeparatedList.TitleColor = Color3.fromRGB(125, 125, 125)
-module.SeparatedList.Item.TitleColor = Color3.fromRGB(150, 150, 150)
-module.SeparatedList.Item.ValueColor = Color3.fromRGB(250, 250, 250)
+module.SeparatedList.TitleColor = Color3.fromRGB(150, 150, 150)
+module.SeparatedList.Item.TitleColor = Color3.fromRGB(0, 0, 0)
+module.SeparatedList.Item.ValueColor = Color3.fromRGB(0, 0, 0)
 
 -- TextField component color
 module.TextField = {}
 module.TextField.TitleColor = module.SeparatedList.TitleColor
-module.TextField.ContentColor = Color3.fromRGB(250, 250, 250)
-module.TextField.PlaceholderColor = Color3.fromRGB(125, 125, 125)
+module.TextField.ContentColor = Color3.fromRGB(0, 0, 0)
+module.TextField.PlaceholderColor = Color3.fromRGB(150, 150, 150)
 
 -- Duration for tweens
 module.Duration = {
