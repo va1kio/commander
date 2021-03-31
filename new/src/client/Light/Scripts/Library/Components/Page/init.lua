12f5939e5ca4aa0ff4df9343ea799b758f8e3b52
va1kio <40730127+va1kio@users.noreply.github.com>
2021-03-31 02:45:14 +0000

    made Page respect Stylesheet and AutomaticCanvasSize

--- new/src/client/Light/Scripts/Library/Components/Page/init.lua
+++ new/src/client/Light/Scripts/Library/Components/Page/init.lua
@@ -4,6 +4,8 @@ module.new = function(Name: string, Parent: instance)
 	local Comp = script.Comp:Clone()
 	Comp.Name = Name
 	Comp.VerticalScrollBarInset = Enum.ScrollBarInset.ScrollBar
+	Comp.ScrollBarImageColor3 = module.Latte.Modules.Stylesheet.Window.ScrollBarColor
+	Comp.AutomaticCanvasSize = Enum.AutomaticSize.Y
 	Comp.Parent = Parent
 	
 	return Comp
