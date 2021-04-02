fad2538de3bd1fd3ce69d839bd702fc0a567df62
va1kio <40730127+va1kio@users.noreply.github.com>
2021-04-02 15:00:27 +0000

    Add OutlineButton

---
+++ new/src/client/Light/Scripts/Library/Components/OutlinedButton/init.lua
@@ -0,0 +1,22 @@
+local module = {}
+
+local reaction = function(Button: guiobject, State: string)
+	module.Latte.Modules.Animator.Button.Flat[State](Button)
+end
+
+module.new = function(Name: string, Text: string?, Parent: instance, Callback: (string) -> void)
+	local Comp = script.Comp:Clone()
+	Comp.Name = Name
+	Comp.Container.Title.Text = Text or Name
+	Comp.BackgroundColor3 = module.Latte.Modules.Stylesheet.ThemeColor
+	Comp.Background.Outline.ImageColor3 = module.Latte.Modules.Stylesheet.ThemeColor
+	
+	module.Latte.Modules.Trigger.new(Comp, reaction, 3, false):Connect(function()
+		Callback()
+	end)
+	
+	Comp.Parent = Parent
+	return Comp
+end
+
+return module
