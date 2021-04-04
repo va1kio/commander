735d2bc1a8577b896c69fd4bb444adfe8d2b714c
va1kio <40730127+va1kio@users.noreply.github.com>
2021-04-04 14:47:18 +0000

    addList REVE support

--- new/src/client/Light/Scripts/Library/Constructors/Window.lua
+++ new/src/client/Light/Scripts/Library/Constructors/Window.lua
@@ -1,4 +1,5 @@
 local module, Latte, Elements, isActive, currentPage = {}, nil, nil, true, nil
+local allLists = {}
 
 module.Toggle = function()
 	module.Window.Toggle()
@@ -15,7 +16,28 @@ module.init = function()
 end
 
 module.setup = function()
-	
+	module.Remotes.RemoteEvent.OnClientEvent:Connect(function(Type, Protocol, Attachment)
+		if Type == "newList" then
+			if allLists[Protocol] then
+				allLists[Protocol].List.Parent = nil
+			else
+				allLists[Protocol] = {
+					Window = Latte.Components.Window.new(Protocol, Protocol, Vector2.new(300, 400), false, Elements)
+				}
+				
+				Latte.Components.Page.new("Body", allLists[Protocol].Window.Pages).UIListLayout:Destroy()
+			end
+			
+			allLists[Protocol]["List"] = Latte.Components.DenseList.new("List", allLists[Protocol].Window.Pages.Body)
+			for i,v in pairs(Attachment) do
+				allLists[Protocol]["List"].Items[i] = v
+			end
+			
+			allLists[Protocol].Window.ZIndex = 2
+			allLists[Protocol].Window.Toggle()
+			allLists[Protocol].Window.switchPage("Body")
+		end
+	end)
 end
 
 return module
