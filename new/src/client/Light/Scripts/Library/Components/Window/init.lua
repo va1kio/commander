aaeefa485bd341a9d254e81a3932ab6d80e73673
va1kio <40730127+va1kio@users.noreply.github.com>
2021-04-05 04:56:22 +0000

    Made Top.BackgroundColor3 respect Stylesheet

--- new/src/client/Light/Scripts/Library/Components/Window/init.lua
+++ new/src/client/Light/Scripts/Library/Components/Window/init.lua
@@ -13,9 +13,11 @@ local function stylise(UI: guiobject)
 		Accent.Parent = UI.Container.Top
 		
 		UI.Container.TopbarShadow.Visible = false
+		UI.Container.Top.BackgroundColor3 = Stylesheet.Window.TopbarBackgroundColorIfAccentUsed
 		UI.Container.Top.Background.ImageColor3 = Stylesheet.Window.TopbarBackgroundColorIfAccentUsed
 		UI.Container.Top.Title.TextColor3 = Stylesheet.Window.TopbarElementColorIfAccentUsed
 	else
+		UI.Container.Top.BackgroundColor3 = Stylesheet.Window.TopbarColor
 		UI.Container.Top.Background.ImageColor3 = Stylesheet.Window.TopbarColor
 		UI.Container.Top.Title.TextColor3 = Stylesheet.Window.TopbarElementsColor
 	end
